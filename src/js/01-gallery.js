bfe528c9831d6594bfeac33e98aa864962026253
Oleksandr Savchenko <19savchenko93@gmail.com>
2022-09-17 15:41:32 +0300

    Bugfix homework#8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 // Change code below this line
 
 console.log(galleryItems);
+console.log(galleryItems);
+
+console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImageCardMarkup(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+       return `
+  <a class="gallery__item" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${original}"
+      alt="${description}"
+    />
+  </a>
+`;})
+    .join('');
+};
+
+    new SimpleLightbox('.gallery a',{
+        captionsData: "alt",
+    })
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener("input", throttle(onTextinput, 500));
+form.addEventListener("submit", onSubmit)
+
+populateText();
+
+function onTextinput(e) {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    console.log("Отправляем форму");
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateText() {
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
+    const email = document.querySelector('.feedback-form input');
+    const message = document.querySelector('.feedback-form textarea');
+    if (savedMessage) {
+        savedMessage = JSON.parse(savedMessage);
+        email.value = savedMessage.email;
+        message.value = savedMessage.message;
+    }
+
+};
 
